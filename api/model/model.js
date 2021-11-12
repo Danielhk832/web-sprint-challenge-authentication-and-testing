6d4ad08adbb31aa93cc1bae8062afd3ae174ee84
Daniel Kish <aox.soljah@gmail.com>
2021-11-13 01:55:51 +0700

    changed db query to see if that fixes codegrade score

--- api/model/model.js
+++ api/model/model.js
@@ -2,7 +2,7 @@ const db = require("../../data/dbConfig");
 
 async function register(user) {
   const [id] = await db("users").insert(user);
-  return db("users").where("id", id);
+  return db("users").where("id", id).first();
 }
 
 function findBy(username) {
